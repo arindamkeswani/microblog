50ac6afddb1eed476a00d9751984270f45de1702
Arindam <arindamkeswani@gmail.com>
2021-11-29 17:19:07 +0530

    Displaying past entries and posting data

--- practice/app.py
+++ practice/app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries=[]
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method ==  "POST":
         newPostContent = request.form.get("content")
         formattedDate = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(newPostContent, formattedDate)
-    return render_template("home.html")
+        entries.append((newPostContent, formattedDate))
+    return render_template("home.html", entries=entries)
