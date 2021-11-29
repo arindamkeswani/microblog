e10ced3be76390f4db40dd391979d98c4b39c92e
Arindam <arindamkeswani@gmail.com>
2021-11-29 13:11:26 +0530

    Jinja2 expressions

--- practice/flaskintro/app.py
+++ practice/flaskintro/app.py
@@ -12,12 +12,12 @@ from jinja2 import Template
 app = Flask(__name__) #dunder name, always unique
 
 #telling the app to register the end point
-@app.route("/") 
-def hello_world():
-    return render_template(
-        "jinja_intro.html",
-        name="Arindam",
-        language_name = "Jinja2")
+# @app.route("/") 
+# def hello_world():
+#     return render_template(
+#         "jinja_intro.html",
+#         name="Arindam",
+#         language_name = "Jinja2")
 
 # #will give error
 # @app.route("/hello") 
@@ -27,3 +27,42 @@ def hello_world():
 # @app.route("/webpage") 
 # def hello_html():
 #     return render_template("index2.html")
+
+@app.route("/expressions/")
+def expressions():
+
+    #interpolation
+    name="Steve"
+    language_name = "Jinja2"
+
+    #Addition, subtraction
+    no_of_oranges=5
+    no_of_apples=7
+    old_quantity=9
+    new_quantity=4
+    first_name="Tony"
+    last_name = "Stark"
+
+
+    # return render_template("jinja_intro.html",
+    #                         name=name,
+    #                         language_name=language_name,
+    #                         no_of_oranges=no_of_oranges,
+    #                         no_of_apples=no_of_apples,
+    #                         old_quantity=old_quantity,
+    #                         new_quantity=new_quantity,
+    #                         first_name=first_name,
+    #                         last_name=last_name)
+
+    kwargs = {
+        "name":name,
+        "language_name":language_name,
+        "no_of_oranges":no_of_oranges,
+        "no_of_apples":no_of_apples,
+        "old_quantity":old_quantity,
+        "new_quantity":new_quantity,
+        "first_name":first_name,
+        "last_name":last_name
+    }
+
+    return render_template("jinja_intro.html", **kwargs)
